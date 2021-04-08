32c57527dd6fa11a0aa5794b10b667df822da62f
qsc-1999 <1150299131@qq.com>
2021-04-08 17:19:39 +0800

    修改test

--- test.py
+++ test.py
@@ -5,13 +5,16 @@ from sklearn.metrics import roc_auc_score
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from data import DeeperDataset,test_transforms
+import numpy as np
 
 # load data
 def load_test():
-    test_root_dir = '/home/liu/deepfake_detection/VIT/data/FF++/test/c23/'
+    test_root_dir = '/home/liu/qsc/VIT/data/FF++/test/c23/'
     test_root_dir = glob.glob(os.path.join(test_root_dir, '*'))
     data_test = []
     for i in range(len(test_root_dir)):
+    #    if i==1 or i==2 or i==3:
+    #        continue
         test_split_dir = test_root_dir[i]
         #print(test_split_dir)
         #print('-----------------------')
@@ -24,20 +27,27 @@ def load_test():
 
 # acc
 def cal_acc(y, y_pred):
-    y.cuda()
-    y_pred.cuda()
+    #y.cuda()
+    #y_pred.cuda()i
+    #y = [1,1]
+    #y_pred = [0,1]
+    #print(y.type())
+    #print(y_pred.type())
     if len(y) != len(y_pred):
         print('their length is not equal!')
         return 0
     else:
-        return (torch.eq(y, y_pred).sum() / len(y)).item()
+        return np.equal(y, y_pred).sum() / len(y)
 
 # auc
 def cal_auc(y, y_pred):
     # y:label [0,0,1,0,1]
     # y_pred: 预测为1时的概率 [:, 1]
-    y.cuda()
-    y_pred.cuda()
+    #y.cuda()
+    #y_pred.cuda()
+    #y = [1,0]
+    #y_pred = [1,0]
+    #print(y)
     if len(y) != len(y_pred):
         print('their length is not equal!')
         return 0
@@ -45,25 +55,46 @@ def cal_auc(y, y_pred):
         return roc_auc_score(y,y_pred)
 
 if __name__ == '__main__':
-    model = torch.load('jx_vit_base_resnet50_384-9fd3c705.pth')
-    #model.cuda()
+    #print(cal_acc())
+    #print(cal_auc())
+    model = torch.load('./Model/model_FF++_HQ_0.9723833799362183.pkl')
+    #model = torch.nn.DataParallel(model)
+    model.cuda()
     test_data = load_test()
     print(len(test_data))
-    label_all = torch.tensor([])
-    pred_all_prob = torch.tensor([])
-    pred_all_cls = torch.tensor([])
+    #label_all = torch.tensor([]).cpu()
+    #pred_all_prob = torch.tensor([]).cpu()
+    #pred_all_cls = torch.tensor([]).cpu()
+    label_all = []
+    pred_all_prob = []
+    pred_all_cls = []
+    k=0
     for i in range(len(test_data)):
-        test_loader = DataLoader(dataset=test_data[i], batch_size=8, shuffle=True)
+        print(k)
+        k+=1
+        #print(len(test_data[i]))
+        test_loader = DataLoader(dataset=test_data[i], batch_size=20, shuffle=True)
         for data, label in test_loader:
-            data = data['image'].cuda()
-            label = label.cuda()
-            label_all += label
+            data = data.cuda(1)
+            label = label.cuda(1)
+            #print(label.shape)
+            #label_all = torch.cat((label_all,label.cpu()))
+            label_all += label.tolist()
+            #print(len(label_all))
             output = model(data)
             output = F.softmax(output,dim=1)
-            pred_all_prob += output[:,1]
-            pred_all_cls += output.argmax(dim=1)
+            pred_cls = output.argmax(dim=1).tolist()
+            pred_prob = output[:,1].tolist()
+            #print(output[:,1].float8())
+            pred_all_prob += pred_prob
+            #pred_all_prob = torch.cat((pred_all_prob,pred_prob.cpu()))
+            pred_all_cls += pred_cls
+            #pred_all_cls = torch.cat((pred_all_cls,output.argmax(dim=1).cpu()))
+    print("enter cal")
     AUC = cal_auc(label_all,pred_all_prob)
     ACC = cal_acc(label_all,pred_all_cls)
+    print("AUC = ",AUC)
+    print("ACC = ",ACC)
 
 
 
